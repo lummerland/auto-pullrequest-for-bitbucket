c7083a76714789569b76e3b707d0896f96bbb300
Manuel Kummerländer <mkummerlaender@seibert-media.net>
2018-03-11 13:16:47 +0100

    Added working hook
    This post commit hook is the trigger to create a new pull request if the
    change does not belong to some protected branches and if the change is
    not covered by an open pullrequest. This is the first working draft that
    needs some refactoring and additional checks.

---
+++ src/main/java/org/lummerland/bitbucket/hook/CommitHook.java
@@ -0,0 +1,79 @@
+package org.lummerland.bitbucket.hook;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.atlassian.bitbucket.hook.repository.PostRepositoryHook;
+import com.atlassian.bitbucket.hook.repository.PostRepositoryHookContext;
+import com.atlassian.bitbucket.hook.repository.RepositoryHookRequest;
+import com.atlassian.bitbucket.pull.PullRequest;
+import com.atlassian.bitbucket.pull.PullRequestSearchRequest;
+import com.atlassian.bitbucket.pull.PullRequestService;
+import com.atlassian.bitbucket.pull.PullRequestState;
+import com.atlassian.bitbucket.repository.Branch;
+import com.atlassian.bitbucket.repository.RefChange;
+import com.atlassian.bitbucket.repository.RefService;
+import com.atlassian.bitbucket.util.Page;
+import com.atlassian.bitbucket.util.PageUtils;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
+@Component
+public class CommitHook implements PostRepositoryHook<RepositoryHookRequest> {
+
+	private static final Logger log = LoggerFactory.getLogger(CommitHook.class);
+
+	private final PullRequestService pullRequestService;
+
+	private final RefService refService;
+
+	@Autowired
+	public CommitHook(
+			@ComponentImport final PullRequestService pullRequestService,
+			@ComponentImport final RefService refService) {
+		this.pullRequestService = pullRequestService;
+		this.refService = refService;
+	}
+
+	@Override
+	public void postUpdate(
+			@Nonnull final PostRepositoryHookContext context,
+			@Nonnull final RepositoryHookRequest request) {
+
+		final Branch defaultBranch = refService.getDefaultBranch(request.getRepository());
+		final List<String> branchesToIgnore = Arrays.asList("refs/heads/develop", "refs/heads/master", defaultBranch.getId());
+
+		request.getRefChanges().stream()
+				.filter(change -> !branchesToIgnore.contains(change.getRef().getId()))
+				.filter(refChange -> !openPullRequestExists(refChange.getRef().getId()))
+				.forEach(refChange -> createPullRequest(request, refChange));
+
+	}
+
+	private boolean openPullRequestExists(final String refId) {
+		final PullRequestSearchRequest pullRequestSearchRequest = new PullRequestSearchRequest.Builder().fromRefId(refId).state(PullRequestState.OPEN).build();
+		final Page<PullRequest> found = pullRequestService.search(pullRequestSearchRequest, PageUtils.newRequest(0, 1));
+		return found.getSize() > 0;
+	}
+
+	private void createPullRequest(final RepositoryHookRequest request, final RefChange change) {
+		pullRequestService.create(
+				"title",
+				"description",
+				new HashSet<String>(),
+				request.getRepository(),
+				change.getRef().getId(),
+				request.getRepository(),
+				refService.getDefaultBranch(request.getRepository()).getId()
+		);
+		log.warn("> Pull Request created");
+	}
+
+}
